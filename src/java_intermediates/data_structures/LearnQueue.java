82414b99ce804162dc3e236642d7e10ecc0a8789
Siheen <siheenmoody@gmail.com>
2022-03-31 14:50:22 -0500

    First Commit

---
+++ src/java_intermediates/data_structures/LearnQueue.java
@@ -0,0 +1,27 @@
+package java_intermediates.data_structures;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LearnQueue {
+    public static void main(String[] args) {
+        Queue<String> myQueue = new LinkedList<>();
+
+        myQueue.offer("Sami");
+        myQueue.offer("Wai");
+        myQueue.offer("Areeg");
+        myQueue.offer("Osama");
+        myQueue.offer("Luc");
+
+        String topElement = myQueue.peek();
+        System.out.println("TOP ELEMENT: " + topElement); // Sami
+
+        String polledElement = myQueue.poll();
+        System.out.println("POLLED (POPPED) ELEMENT: " + polledElement); // Sami
+
+        topElement = myQueue.peek();
+        System.out.println("TOP ELEMENT: " + topElement); // Wai
+
+    }
+
+}
---
+++ src/java_intermediates/enums/Directions.java
@@ -0,0 +1,5 @@
+package java_intermediates.enums;
+
+public enum Directions {
+    NORTH, SOUTH, EAST, WEST
+}
